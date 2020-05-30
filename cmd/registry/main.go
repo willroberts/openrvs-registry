5039af6c6668978960cd20e46af422c29731c566
ijemafe <noreply@ijemafe.com>
2020-05-30 10:40:33 -0700

    Checkpoint

--- cmd/registry/main.go
+++ cmd/registry/main.go
@@ -1,5 +1,7 @@
 // Temporary entrypoint for development. Convert to proper entrypoint later by
 // moving libraries into package registry.
+//todo: checkpoint allservers to disk every 5 mins
+//todo: load initial allservers from disk on init
 package main
 
 import (
@@ -16,6 +18,32 @@ const HealthCheckInterval = 1 * time.Minute
 const PassedCheckThreshold = 3
 const FailedCheckThreshold = 3
 const MaxFailedChecks = 2880 // 2 days.
+
+var typeMap = map[string]string{
+	// Raven Shield modes
+	"RGM_BombAdvMode":           "adv",  // Bomb
+	"RGM_DeathmatchMode":        "adv",  // Survival
+	"RGM_EscortAdvMode":         "adv",  // Pilot
+	"RGM_HostageRescueAdvMode":  "adv",  // Hostage
+	"RGM_HostageRescueCoopMode": "coop", // Hostage Rescue
+	"RGM_HostageRescueMode":     "coop",
+	"RGM_MissionMode":           "coop", // Mission
+	"RGM_SquadDeathmatch":       "adv",
+	"RGM_SquadTeamDeathmatch":   "adv",
+	"RGM_TeamDeathmatchMode":    "adv",  // Team Survival
+	"RGM_TerroristHuntCoopMode": "coop", // Terrorist Hunt
+	"RGM_TerroristHuntMode":     "coop",
+
+	// Athena Sword modes
+	"RGM_CaptureTheEnemyAdvMode": "adv",
+	"RGM_CountDownMode":          "coop",
+	"RGM_KamikazeMode":           "adv",
+	"RGM_ScatteredHuntAdvMode":   "adv",
+	"RGM_TerroristHuntAdvMode":   "adv",
+
+	// TODO: Add Iron Wrath modes
+	// Free Backup, Gas Alert, Intruder, Limited Seats, Virus Upload (all adv)
+}
 
 // Temporary globals for development.
 var (
@@ -68,8 +96,10 @@ func main() {
 
 	// Do some UDP testing.
 	go func() {
-		time.Sleep(2 * time.Second)
-		testUDP()
+		for {
+			time.Sleep(5 * time.Second)
+			testUDP()
+		}
 	}()
 
 	// Start listening on TCP/8080 for HTTP requests from OpenRVS clients.
@@ -106,16 +136,28 @@ func parseUDPMessage(ip string, msg []byte) {
 	}
 
 	for _, s := range servers { //todo: use a map of "ip:port" keys
-		if (s.IP == report.IPAddress) && (s.Port == report.Port) {
-			return // Server already known.
+		// When testing locally, key on Server Name instead of IP+Port.
+		if report.IPAddress == "127.0.0.1" {
+			if (s.Name == report.ServerName) && (s.Port == report.Port) {
+				return // don't parse udp message, only update healthcheck
+			}
+			continue
 		}
-		servers = append(servers, server{
-			Name:     report.ServerName,
-			IP:       report.IPAddress,
-			Port:     report.Port,
-			GameMode: report.CurrentMode, //fixme: map to coop/adv
-		})
+		// End local testing code.
+
+		if (s.IP == report.IPAddress) && (s.Port == report.Port) {
+			return //don't parse udp message, only update healthcheck
+		}
 	}
+
+	servers = append(servers, server{
+		Name:     report.ServerName,
+		IP:       report.IPAddress,
+		Port:     report.Port,
+		GameMode: gameModeToType(report.CurrentMode),
+	})
+
+	log.Printf("there are now %d registered servers (confirm over http)", len(servers))
 }
 
 func healthcheck(s server) {
@@ -170,7 +212,8 @@ func loadTestServers() {
 
 func testUDP() {
 	// Pretend to be a server and replay the beacon.
-	log.Printf("sending %d udp beacons", len(servers))
+	// Note: Always comes from 127.0.0.1, which breaks IP detection
+	log.Printf("sending %d udp beacons", len(testservers))
 	for _, s := range testservers {
 		bytes, err := beacon.GetServerReport(s.IP, s.Port+1000)
 		if err != nil {
@@ -187,9 +230,9 @@ func testUDP() {
 			log.Println("testudp->write error:", err)
 			continue
 		}
-		log.Println("beacon sent")
 	}
 }
 
-//todo: checkpoint allservers to disk every 5 mins
-//todo: load initial allservers from disk on init
+func gameModeToType(m string) string {
+	return typeMap[m]
+}
