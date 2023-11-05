5aeef8b6c2a65c1c5946b31beb367cc6a5a5ab66
Will Roberts <willroberts3@protonmail.com>
2023-11-05 11:08:32 -0500

    Adds GH tests

--- latest.go
+++ latest.go
@@ -10,9 +10,6 @@ import (
 // latestReleaseURL contains the latest release info for OpenRVS.
 const latestReleaseURL = "https://api.github.com/repos/OpenRVS-devs/OpenRVS/releases/latest"
 
-// defaultVersion is a fallback version string.
-var defaultVersion = []byte("unknown")
-
 // githubResponse is the JSON structure we want to parse.
 type githubResponse struct {
 	TagName string `json:"tag_name"`
@@ -25,7 +22,7 @@ func GetLatestReleaseVersion() []byte {
 	resp, err := http.Get(latestReleaseURL)
 	if err != nil {
 		log.Println("error getting version from github:", err)
-		return defaultVersion
+		return []byte("unknown")
 	}
 	defer resp.Body.Close()
 
@@ -33,14 +30,14 @@ func GetLatestReleaseVersion() []byte {
 	bytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println("error reading response body:", err)
-		return defaultVersion
+		return []byte("unknown")
 	}
 
 	// Parse the JSON, storing it directly in githubResponse r.
 	var r githubResponse
 	if err := json.Unmarshal(bytes, &r); err != nil {
 		log.Println("error unmarshaling json:", err)
-		return defaultVersion
+		return []byte("unknown")
 	}
 
 	// Return just the latest version tag as []byte.
